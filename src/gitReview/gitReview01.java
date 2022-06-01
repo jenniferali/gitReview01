a890c9896f7b815ef0b895c7d6d7e9ead071db2d
jenniferali <jenniferoali@gmail.com>
2022-05-31 20:52:37 -0500

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -3,8 +3,13 @@ package gitReview;
 public class gitReview01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.print("Today we learn github!");
+		
+		System.out.println("Today is a very good day!");
+		
+		System.out.println("Today is a very good day!");
+		
 	}
 
 }
